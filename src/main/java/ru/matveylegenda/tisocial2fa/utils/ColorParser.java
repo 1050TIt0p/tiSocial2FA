4743b195272ad1bd9c7258d04ff74e95610f9608
1050TIt0p <160386710+1050TIt0p@users.noreply.github.com>
2024-06-19 18:29:54 +0300

    Optimize color parser

--- src/main/java/ru/matveylegenda/tisocial2fa/utils/ColorParser.java
+++ src/main/java/ru/matveylegenda/tisocial2fa/utils/ColorParser.java
@@ -1,25 +1,31 @@
 package ru.matveylegenda.tisocial2fa.utils;
 
-import net.md_5.bungee.api.ChatColor;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import net.md_5.bungee.api.ChatColor;
+
+import static net.md_5.bungee.api.ChatColor.COLOR_CHAR;
 
 public class ColorParser {
     private static final Pattern HEX_PATTERN = Pattern.compile("&#([a-fA-F\\d]{6})");
 
     public static String hex(String message) {
         Matcher matcher = HEX_PATTERN.matcher(message);
-        StringBuffer buffer = new StringBuffer(message.length() + 4 * 8);
-
+        StringBuilder builder = new StringBuilder(message.length() + 4 * 8);
         while (matcher.find()) {
-            String hexCode = matcher.group(1);
-            ChatColor color = ChatColor.of("#" + hexCode);
-            String replacement = color.toString();
-            matcher.appendReplacement(buffer, replacement);
+            String group = matcher.group(1);
+            matcher.appendReplacement(builder,
+                    COLOR_CHAR + "x" +
+                            COLOR_CHAR + group.charAt(0) +
+                            COLOR_CHAR + group.charAt(1) +
+                            COLOR_CHAR + group.charAt(2) +
+                            COLOR_CHAR + group.charAt(3) +
+                            COLOR_CHAR + group.charAt(4) +
+                            COLOR_CHAR + group.charAt(5));
         }
+        message = matcher.appendTail(builder).toString();
 
-        matcher.appendTail(buffer);
-        return ChatColor.translateAlternateColorCodes('&', buffer.toString());
+        return ChatColor.translateAlternateColorCodes('&', message);
     }
 }
